cc354377db344941475effeeb97f3fc583da9325
Denis Beresnev <shanthi.bunch@gmail.com>
2023-01-06 16:40:32 +0300

    ep1-fillRect

--- scriptLesson1.js
+++ scriptLesson1.js
@@ -4,3 +4,8 @@ canvas.width = 800;
 canvas.height = window.innerHeight;
 
 const c = canvas.getContext("2d");
+
+c.fillRect(100, 100, 50, 50);
+c.fillRect(400, 100, 300, 50);
+c.fillRect(200, 200, 200, 200);
+c.fillRect(300, 300, 300, 300);
