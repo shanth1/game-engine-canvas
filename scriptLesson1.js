b2db172f9aeaa1b70263904651b77c03f5d50e42
Denis Beresnev <shanthi.bunch@gmail.com>
2023-01-06 16:43:05 +0300

    ep2-fillStyle

--- scriptLesson1.js
+++ scriptLesson1.js
@@ -5,7 +5,10 @@ canvas.height = window.innerHeight;
 
 const c = canvas.getContext("2d");
 
+c.fillStyle = "#48D1CC";
 c.fillRect(100, 100, 50, 50);
+c.fillStyle = "#00fa9a";
 c.fillRect(400, 100, 300, 50);
 c.fillRect(200, 200, 200, 200);
+c.fillStyle = "rgba(255, 0, 0, 0.5)";
 c.fillRect(300, 300, 300, 300);
