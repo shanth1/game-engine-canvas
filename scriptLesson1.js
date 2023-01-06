8793f1f7dcf3b191626ee0862ccc2fdaa3101965
Denis Beresnev <shanthi.bunch@gmail.com>
2023-01-06 18:06:26 +0300

    ep3-simple-animation
    requestAnimationFrame
    simple movement without cleaning

--- scriptLesson1.js
+++ scriptLesson1.js
@@ -1,34 +1,20 @@
 const canvas = document.getElementById("lesson_1");
 
-canvas.width = 800;
-canvas.height = window.innerHeight;
+canvas.width = window.innerWidth * 0.8;
+canvas.height = 600;
 
 const c = canvas.getContext("2d");
 
-c.fillStyle = "#48D1CC";
-c.fillRect(100, 100, 50, 50);
-c.fillStyle = "#00fa9a";
-c.fillRect(400, 100, 300, 50);
-c.fillRect(200, 200, 200, 200);
-c.fillStyle = "rgba(255, 0, 0, 0.5)";
-c.fillRect(300, 300, 300, 300);
+let x = 100;
 
-c.beginPath();
-c.moveTo(50, 500);
-c.lineTo(100, 100);
-c.lineTo(300, 200);
-c.lineTo(600, 300);
-c.lineTo(600, 600);
-c.lineTo(700, 600);
-c.strokeStyle = "rgba(0, 0, 200, 0.5)";
-c.stroke();
+const animate = () => {
+    requestAnimationFrame(animate);
 
-const colors = ["#441538", "#853c4f", "#243571", "#9680b9", "#ffd8ff"];
-for (let i = 0; i < 50; i++) {
-    const x = Math.random() * canvas.width;
-    const y = Math.random() * canvas.height;
     c.beginPath();
-    c.arc(x, y, 50, 0, Math.PI * 2);
-    c.strokeStyle = colors[Math.floor(Math.random() * colors.length)];
+    c.arc(x, canvas.height / 2, 20, 0, Math.PI * 2);
     c.stroke();
-}
+
+    x += 1;
+};
+
+animate();
