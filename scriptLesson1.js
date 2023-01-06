60f9fb3763a8e17142490dd2a07a85a9271d4d81
Denis Beresnev <shanthi.bunch@gmail.com>
2023-01-06 21:10:44 +0300

    ep3-bounds-and-random
    added bounds
    added dx, dy
    get random values for x, y, dx, dy

--- scriptLesson1.js
+++ scriptLesson1.js
@@ -5,19 +5,32 @@ canvas.height = 600;
 
 const c = canvas.getContext("2d");
 
-let x = 100;
+const radius = 20;
+let x = radius + Math.random() * (canvas.width - 2 * radius);
+let y = radius + Math.random() * (canvas.height - 2 * radius);
+let dx = Math.random() < 0.5 ? -Math.random() * 4 - 2 : Math.random() * 4 + 2;
+let dy = Math.random() < 0.5 ? -Math.random() * 4 - 2 : Math.random() * 4 + 2;
 
-const clear = () => c.clearRect(0, 0, innerWidth, innerHeight);
+const clear = () => c.clearRect(0, 0, canvas.width, canvas.height);
 
 const animate = () => {
     requestAnimationFrame(animate);
     clear();
 
     c.beginPath();
-    c.arc(x, canvas.height / 2, 20, 0, Math.PI * 2);
+    c.arc(x, y, radius, 0, Math.PI * 2);
     c.stroke();
 
-    x += 1;
+    if (y + radius > canvas.height || y - radius < 0) {
+        dy = -dy;
+    }
+
+    if (x + radius > canvas.width || x - radius < 0) {
+        dx = -dx;
+    }
+
+    x += dx;
+    y += dy;
 };
 
 animate();
