7188943e1cbc2c99f33566920a1d130ac84851d7
Denis Beresnev <shanthi.bunch@gmail.com>
2023-01-07 23:40:16 +0300

    fixed-random-methods

--- gameEngine.js
+++ gameEngine.js
@@ -1,13 +1,17 @@
 const radius = 20;
 
-const getRandomNumberOfRange = (min, max) => Math.random() * (max - min) - min;
+const getRandomNumberOfRange = (min, max) => Math.random() * (max - min) + min;
 
 const getRandomSign = () => (Math.random() < 0.5 ? -1 : 1);
 
-const getRandomVelocityVector = (min, max) =>
-    getRandomSign() * getRandomNumberOfRange(min, max);
+const getRandomVelocityVector = (min, max) => [
+    getRandomSign() * getRandomNumberOfRange(min, max),
+    getRandomSign() * getRandomNumberOfRange(min, max),
+];
 
-const getRandomPosition = (size, radius) =>
-    radius + Math.random() * (size - 2 * radius);
+const getRandomPosition = (canvas, radius) => [
+    radius + Math.random() * (canvas.width - 2 * radius),
+    radius + Math.random() * (canvas.height - 2 * radius),
+];
 
 const clear = () => c.clearRect(0, 0, canvas.width, canvas.height);
