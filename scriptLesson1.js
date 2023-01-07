853583caf6f711b9647528bd93df223f536c20f6
Denis Beresnev <shanthi.bunch@gmail.com>
2023-01-07 15:00:57 +0300

    ep3-circle-constructor
    added Circle class with optional pos and vec args
    fixed random methods
    added draw and update methods in Circle class

--- scriptLesson1.js
+++ scriptLesson1.js
@@ -5,32 +5,61 @@ canvas.height = 600;
 
 const c = canvas.getContext("2d");
 
-const radius = 20;
-let x = radius + Math.random() * (canvas.width - 2 * radius);
-let y = radius + Math.random() * (canvas.height - 2 * radius);
-let dx = Math.random() < 0.5 ? -Math.random() * 4 - 2 : Math.random() * 4 + 2;
-let dy = Math.random() < 0.5 ? -Math.random() * 4 - 2 : Math.random() * 4 + 2;
-
 const clear = () => c.clearRect(0, 0, canvas.width, canvas.height);
+
+const getRandomNumberOfRange = (min, max) => Math.random() * (max - min) + min;
+
+const getRandomSign = () => (Math.random() < 0.5 ? -1 : 1);
+
+const getRandomVelocityVector = (min, max) => [
+    getRandomSign() * getRandomNumberOfRange(min, max),
+    getRandomSign() * getRandomNumberOfRange(min, max),
+];
+
+const getRandomPosition = (canvas, radius) => [
+    radius + Math.random() * (canvas.width - 2 * radius),
+    radius + Math.random() * (canvas.height - 2 * radius),
+];
+
+class Circle {
+    constructor(
+        radius,
+        [x, y] = getRandomPosition(canvas, radius),
+        [dx, dy] = getRandomVelocityVector(2, 4),
+    ) {
+        this.radius = radius;
+        this.x = x;
+        this.y = y;
+        this.dx = dx;
+        this.dy = dy;
+    }
+
+    draw = () => {
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        c.stroke();
+    };
+
+    update = () => {
+        if (this.y + this.radius > canvas.height || this.y - this.radius < 0) {
+            this.dy = -this.dy;
+        }
+        if (this.x + this.radius > canvas.width || this.x - this.radius < 0) {
+            this.dx = -this.dx;
+        }
+
+        this.x += this.dx;
+        this.y += this.dy;
+    };
+}
+
+const circle = new Circle(20);
 
 const animate = () => {
     requestAnimationFrame(animate);
     clear();
-
-    c.beginPath();
-    c.arc(x, y, radius, 0, Math.PI * 2);
-    c.stroke();
-
-    if (y + radius > canvas.height || y - radius < 0) {
-        dy = -dy;
-    }
-
-    if (x + radius > canvas.width || x - radius < 0) {
-        dx = -dx;
-    }
-
-    x += dx;
-    y += dy;
+    circle.draw();
+    circle.update();
 };
 
 animate();
