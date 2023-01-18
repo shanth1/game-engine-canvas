035ea1b45e49189fb6b586aedbb2f68379d55b2c
Denis Beresnev <shanthi.bunch@gmail.com>
2023-01-19 01:58:20 +0300

    matrix-definition

---
+++ gameEngine/utils/Math/Matrix.js
@@ -0,0 +1,31 @@
+/*
+    Linear transformation is a function with lineal rules
+    transformation - function
+
+    Linear:
+    1. Lines remain lines
+    2. Origin remains fixed
+
+    Grid lines remain parallel and evenly spaces
+
+    2x2 Matrix. Columns define basis vector (i, j)
+
+    [[ix, jx], [iy, jy ]]
+
+    Multiplying a vector by a matrix allows you to understand
+    what the transformation does with the vector
+*/
+
+/**
+ * The product of two matrices (space transformations) is the composition matrix
+ * (the result of transformation)
+ */
+
+/**
+ * The determinant of the transformation
+ * determinant is coefficient of changing area of transformation
+ */
+
+export class Matrix {
+    constructor() {}
+}
