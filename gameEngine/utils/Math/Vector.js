7657747e33be325b9caa05faef9e2d42df078e52
Denis Beresnev <shanthi.bunch@gmail.com>
2023-01-18 17:05:29 +0300

    methods-of-Vector-class
    magnitude
    add
    subtract
    scaling
    getUnitVector
    
    basis for drawVector method

--- gameEngine/utils/Math/Vector.js
+++ gameEngine/utils/Math/Vector.js
@@ -1,1 +1,21 @@
-export class Vector {}
+export class Vector {
+    constructor(x, y) {
+        // origin
+        this.x = x;
+        this.y = y;
+        this.magnitude = Math.abs(this.x ** 2, this.y ** 2);
+    }
+
+    add = (vec) => new Vector(this.x + vec.x, this.y + vec.y);
+    subtract = (vec) => new Vector(this.x - vec.x, this.y - vex.y);
+    scaling = (scalar) => new Vector(this.x * scalar, this.y * scalar);
+    getUnitVector = () => {
+        return this.magnitude === 0
+            ? new Vector(0, 0)
+            : this.x === 0 || this.y === 0
+            ? this.magnitude(this.x, this.y)
+            : new Vector(this.x / this.magnitude, this.y / this.magnitude);
+    };
+
+    drawVector = () => {};
+}
