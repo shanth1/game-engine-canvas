a59b1ebbe8cf1cfd88968a7130e39476a0658394
Denis Beresnev <shanthi.bunch@gmail.com>
2023-01-20 00:40:45 +0300

    redesigned-some-vector-methods
    remove this.magnitude
    added getMagnitude and getSquareMagnitude
    renamed getDotProduct to dotProduct

--- gameEngine/utils/Math/Vector.js
+++ gameEngine/utils/Math/Vector.js
@@ -3,12 +3,15 @@ export class Vector {
         // origin
         this.x = x;
         this.y = y;
-        this.magnitude = Math.abs(this.x ** 2, this.y ** 2);
     }
     add = (vec) => new Vector(this.x + vec.x, this.y + vec.y);
     subtract = (vec) => new Vector(this.x - vec.x, this.y - vex.y);
     scale = (scalar) => new Vector(this.x * scalar, this.y * scalar);
-    getDotProduct = (vec) => new Vector(this.x * vec.x, this.y * vec.y);
+    dotProduct = (vec) => new Vector(this.x * vec.x, this.y * vec.y);
+
+    getMagnitude = () => Math.abs(this.x ** 2 + this.y ** 2);
+    getSquareMagnitude = () => this.x ** 2 + this.y ** 2;
+
     getUnitVector = () => {
         return this.magnitude === 0
             ? new Vector(0, 0)
