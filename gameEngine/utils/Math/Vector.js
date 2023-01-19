4b8c7a648636a4cb7c67928ae289f10317015926
Denis Beresnev <shanthi.bunch@gmail.com>
2023-01-20 01:01:10 +0300

    vec2-vec3

--- gameEngine/utils/Math/Vector.js
+++ gameEngine/utils/Math/Vector.js
@@ -1,4 +1,4 @@
-export class Vector {
+export class Vec2 {
     constructor(x, y) {
         // origin
         this.x = x;
@@ -13,12 +13,31 @@ export class Vector {
     getSquareMagnitude = () => this.x ** 2 + this.y ** 2;
 
     getUnitVector = () => {
-        return this.magnitude === 0
-            ? new Vector(0, 0)
-            : this.x === 0 || this.y === 0
-            ? this.magnitude(this.x, this.y)
+        return this.x === 0 || this.y === 0
+            ? Math.max(this.x, this.y)
             : new Vector(this.x / this.magnitude, this.y / this.magnitude);
     };
 
     drawVector = () => {};
+}
+
+export class Vec3 extends Vec2 {
+    constructor(x, y, z) {
+        super(x, y);
+        this.z = z;
+    }
+
+    add = (vec) => new Vector(this.x + vec.x, this.y + vec.y, this.z + vec.z);
+    subtract = (vec) =>
+        new Vector(this.x - vec.x, this.y - vex.y, this.z - vec.z);
+    scale = (scalar) =>
+        new Vector(this.x * scalar, this.y * scalar, this.z * z);
+    dotProduct = (vec) =>
+        new Vector(this.x * vec.x, this.y * vec.y, this.z * vec.z);
+
+    getMagnitude = () => Math.abs(this.x ** 2 + this.y ** 2 + this.z ** 2);
+    getSquareMagnitude = () => this.x ** 2 + this.y ** 2 + this.z ** 2;
+
+    getUnitVector = () =>
+        new Vector(this.x / this.magnitude, this.y / this.magnitude);
 }
